dd853712a928556ae74b90dde368daa74184e71b
FeliciaMarlove <missflorencemary@hotmail.com>
2020-05-30 11:03:19 +0200

    xhr interceptor

--- 30jr_CRUD/mobile-app/src/app/_Security/xhr-interceptor.ts
+++ 30jr_CRUD/mobile-app/src/app/_Security/xhr-interceptor.ts
@@ -1,2 +1,14 @@
-export class XhrInterceptor {
+import {Injectable} from '@angular/core';
+import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class XhrInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        const xhr = req.clone({
+            headers: req.headers.set('authorization', 'Basic ' + sessionStorage.getItem('auth'))
+        });
+        return next.handle(xhr);
+    }
 }
