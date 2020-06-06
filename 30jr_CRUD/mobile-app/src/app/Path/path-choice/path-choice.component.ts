1772fc5b9e8afb7b2f4a5ee4943dd6e9e3eb09d1
FeliciaMarlove <missflorencemary@hotmail.com>
2020-06-06 11:46:13 +0200

    Select a path + pathService

--- 30jr_CRUD/mobile-app/src/app/Path/path-choice/path-choice.component.ts
+++ 30jr_CRUD/mobile-app/src/app/Path/path-choice/path-choice.component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PopoverComponent} from '../../_Utils/popover/popover.component';
+import {PopoverController} from '@ionic/angular';
+import {Path} from '../../_Models/path';
+import {PathService} from '../../_Services/path.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-path-choice',
-  templateUrl: './path-choice.component.html',
-  styleUrls: ['./path-choice.component.scss'],
+    selector: 'app-path-choice',
+    templateUrl: './path-choice.component.html',
+    styleUrls: ['./path-choice.component.scss'],
 })
 export class PathChoiceComponent implements OnInit {
+    private selectedPath: Path;
+    private paths: Path[];
 
-  constructor() { }
+    constructor(
+        private popoverController: PopoverController,
+        private pathService: PathService,
+        private router: Router
+    ) {
+    }
 
-  ngOnInit() {}
+    ngOnInit() {
+      this.pathService.readPaths().subscribe(
+        paths => {
+          this.paths = paths;
+      }
+    );
+    }
 
+    async presentPopover(ev: any, path: Path) {
+        const popover = await this.popoverController.create({
+            // component where the template is :
+            component: PopoverComponent,
+            // pass parameters to the component :
+            componentProps: {content: path.pathLongDescription},
+            // don't make the backdrop (background behind) grey :
+            showBackdrop: false,
+            cssClass: 'popover',
+            event: ev,
+            translucent: true
+        });
+        return await popover.present();
+    }
+
+  onStart() {
+    this.pathService.startPath(this.selectedPath.pathId, (Number)(sessionStorage.getItem('user'))).subscribe(response => {
+          if (response.msg) {
+            console.log(response.msg);
+          } else {
+            this.router.navigateByUrl('/dashboard/task');
+          }
+        }, error => console.log(error.toString())
+    );
+  }
+
+  onSelect(path: Path) {
+    this.selectedPath = path;
+  }
 }
--- 30jr_CRUD/mobile-app/src/app/_Services/path.service.ts
+++ 30jr_CRUD/mobile-app/src/app/_Services/path.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Path} from '../_Models/path';
+
+const URI = 'http://localhost:8080/api/userpath/';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PathService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    public readPaths(): Observable<Path[]> {
+        return this.http.get<any>(URI + 'paths');
+    }
+
+  public startPath(pathId: number, userId: number): Observable<any> {
+    const DTO = {pathId, userId};
+    return this.http.post(URI, DTO);
+  }
 }
