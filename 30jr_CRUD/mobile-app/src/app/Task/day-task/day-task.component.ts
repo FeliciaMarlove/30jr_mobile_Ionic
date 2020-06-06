756e32b140218f2693c93c7995e02fa699723e89
FeliciaMarlove <missflorencemary@hotmail.com>
2020-06-06 13:59:24 +0200

    Will try to make a push

--- 30jr_CRUD/mobile-app/src/app/Task/day-task/day-task.component.ts
+++ 30jr_CRUD/mobile-app/src/app/Task/day-task/day-task.component.ts
@@ -39,7 +39,7 @@ export class DayTaskComponent implements OnInit {
     this.userService.getDay(this.userId).subscribe( response => {
       if (response != null) {
         switch (response) {
-          case 3 : this.presentModal(10); break;
+          case 10 : this.presentModal(10); break;
           case 20 : this.presentModal(20); break;
           case 30 : this.presentModal(30); break;
         }
